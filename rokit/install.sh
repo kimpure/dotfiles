abbd736f6283ab4bd77005457f18c9e6c847e06f
sunwoo <161453450+kimpure@users.noreply.github.com>
2025-07-30 19:50:31 +0900

    Update install.sh

--- rokit/install.sh
+++ rokit/install.sh
@@ -1,22 +1,8 @@
-if command -v rokit >/dev/null 2>&1; then
-else
-    echo "Install start rokit"
+# windows
+# https://raw.githubusercontent.com/rojo-rbx/rokit/main/scripts/install.ps1 | Invoke-Expression
 
-    case "$(uname -s)" in
-        Linux*)
-            curl -sSf https://raw.githubusercontent.com/rojo-rbx/rokit/main/scripts/install.sh | bash
-            ;;
-        Darwin*)
-            curl -sSf https://raw.githubusercontent.com/rojo-rbx/rokit/main/scripts/install.sh | bash
-            ;;
-        CYGWIN*|MINGW*|MSYS*)
-            Invoke-RestMethod https://raw.githubusercontent.com/rojo-rbx/rokit/main/scripts/install.ps1 | Invoke-Expression
-            ;;
-        *)
-            echo "Unknown OS: $(uname -s)"
-            ;;
-    esac
-fi
+# linux, macos
+# curl -sSf https://raw.githubusercontent.com/rojo-rbx/rokit/main/scripts/install.sh | bash
 
 rokit add --global rojo-rbx/rojo
 rokit add --global JohnnyMorganz/luau-lsp
