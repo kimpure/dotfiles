006a1b322f7e26ad5ce5dc15f2356d67833aefb0
sunwoo <161453450+kimpure@users.noreply.github.com>
2025-08-02 23:30:48 +0900

    Update install.sh

--- rokit/install.sh
+++ rokit/install.sh
@@ -5,6 +5,8 @@
 # curl -sSf https://raw.githubusercontent.com/rojo-rbx/rokit/main/scripts/install.sh | bash
 
 rokit add --global rojo-rbx/rojo
+rokit add --global lune-org/lune
+
 rokit add --global JohnnyMorganz/luau-lsp
 rokit add --global JohnnyMorganz/StyLua
 rokit add --global Kampfkarren/selene
